fabb88fb2ea946ee1014e7fdf2b54e5359483116
Yusheng Cai <cys9741@seas.upenn.edu>
2021-07-26 22:33:38 -0400

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include "tools/InputParser.h"
+#include "tools/XdrWrapper.h"
+
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello world." << std::endl;
+    std::cout << "type" << std::endl;
+    InputParser ip;
+    std::string fname = argv[1];
+
+    ParameterPack pp;
+    ip.ParseFile(fname,pp);
+
+    auto xtcpp = pp.findParamPack("xtc_file", ParameterPack::KeyType::Required);
+
+    std::string xtcstr;
+    std::string type;
+    xtcpp->ReadString("type", ParameterPack::KeyType::Required,type);
+    xtcpp->ReadString("name", ParameterPack::KeyType::Required, xtcstr);
+
     return 0;
 };
