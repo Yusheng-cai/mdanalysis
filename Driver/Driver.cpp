faeb2aea9b9fe4934e10486c38e81d802c1515d1
Yusheng Cai <cys9741@seas.upenn.edu>
2021-07-29 16:48:47 -0400

    Updated driver code where now we have a ProbeVolumeRegistry that registers all the probeVolumes

--- Driver/Driver.cpp
+++ Driver/Driver.cpp
@@ -7,29 +7,37 @@ Driver::Driver(std::string filename)
 
     auto pv_pack = pack_.findParamPacks("probevolume", ParameterPack::KeyType::Optional);
     auto op_pack = pack_.findParamPacks("orderparameter", ParameterPack::KeyType::Optional);
-    auto xdr_pack= pack_.findParamPack("xdrfiles", ParameterPack::KeyType::Optional);
+    // Make sure that there is only one xdr file
+    auto xdr_pack= pack_.findParamPack("xdrfile", ParameterPack::KeyType::Required);
 
-    if (xdr_pack != nullptr)
-        initializeXdr(xdr_pack);
+    initializeXdr(xdr_pack);
+
+    if (pv_pack.size() != 0)
+    {
+        initializeProbeVolume(pv_pack);
+    }
+
+    if (op_pack.size() != 0)
+    {
+        initializeOP(op_pack);
+    }
 }
 
 void Driver::initializeXdr(const ParameterPack* xdrpack)
 {
-    std::vector<std::string> paths;
-
     std::string path;
-    std::string name;
     std::string mode;
+
+    // Read the path, name, mode of the file
     xdrpack->ReadString("path", ParameterPack::KeyType::Required, path);
-    xdrpack->ReadString("name", ParameterPack::KeyType::Required, name);
     bool read = xdrpack->ReadString("mode", ParameterPack::KeyType::Optional, mode);
 
     // split the path to obtain the type of xdr file we are working with, i.e test.xtc
     int found = path.find_first_of(".");
     std::string type = path.substr(found+1);
 
+    // Create a pointer to the Xdr file that we are working with
     Xdr_ = XdrPtr(XdrFiles::factory::instance().create(type));
-    SimState_ = statePtr(new SimulationState());
 
     // default is reading mode
     if (read == false)
@@ -57,12 +65,35 @@ void Driver::initializeXdr(const ParameterPack* xdrpack)
     }
 }
 
-void initializeProbeVolume(std::vector<const ParameterPack*> PVpack)
+void Driver::initializeProbeVolume(std::vector<const ParameterPack*>& PVpack)
 {
     for (int i=0; i< PVpack.size();i++)
     {
         auto pack = PVpack[i];
         std::string pv_type;
+        std::string pv_name;
         pack->ReadString("type", ParameterPack::KeyType::Required, pv_type);
+        pack->ReadString("name", ParameterPack::KeyType::Required, pv_name);
+
+        ProbeVolumeInput PV_input{*pack, simstate_}; 
+        auto pv_ptr = ProbeVolumes::Factory::instance().create(pv_type, PV_input);
+
+        pv_registry_.registerProbeVolume(pv_name, pv_ptr);
+    }
+}
+
+void Driver::initializeOP(std::vector<const ParameterPack*>& OPpack)
+{
+    for (int i=0;i<OPpack.size();i++)
+    {
+        auto pack = OPpack[i];
+        std::string op_type;
+
+        pack -> ReadString("type", ParameterPack::KeyType::Required, op_type); 
+        OrderParametersInput OP_input{*pack, simstate_, pv_registry_};
+
+        OPptr op_pointer = OPptr(OrderParametersRegistry::Factory::instance().create(op_type, OP_input));
+
+        OP_.push_back(std::move(op_pointer));
     }
 }
--- Driver/Driver.h
+++ Driver/Driver.h
@@ -1,6 +1,7 @@
 #include "OrderParameters/SimulationState.h"
 #include "OrderParameters/OrderParameters.h"
 #include "OrderParameters/ProbeVolume.h"
+#include "OrderParameters/ProbeVolumeRegistry.h"
 #include "xdr/XdrWrapper.h"
 #include "tools/CommonTypes.h"
 #include "tools/InputParser.h"
@@ -16,18 +17,18 @@ class Driver
         using ProbeVolumePtr = std::unique_ptr<ProbeVolume>;
         using OPptr = std::unique_ptr<OrderParameters>;
         using XdrPtr = std::unique_ptr<XdrWrapper>;
-        using statePtr = std::unique_ptr<SimulationState>;
 
         Driver(std::string filename);
         ~Driver(){};
 
         void initializeXdr(const ParameterPack*);
-        void initializeOP(std::vector<const ParameterPack*>);
-        void initializeProbeVolume(std::vector<const ParameterPack*>);
+        void initializeOP(std::vector<const ParameterPack*>&);
+        void initializeProbeVolume(std::vector<const ParameterPack*>&);
     private:
         ParameterPack pack_;
-        std::vector<ProbeVolumePtr> PV_;
+        std::map<std::string, ProbeVolumePtr> MapName2PV_;
         std::vector<OPptr> OP_;
         XdrPtr Xdr_;
-        statePtr SimState_;
+        SimulationState simstate_;
+        ProbeVolumeRegistry pv_registry_;
 };
