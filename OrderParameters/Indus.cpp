26a6aceaf56693577ba1a1cb7dda0005bf7c4ebe
Yusheng Cai <cys9741@seas.upenn.edu>
2021-07-30 14:49:06 -0400

    Added code in INdus for calculate and update, calculate is for calculating the OrderParameter and update is for updating everything that's needed, e.g. probevolume

--- OrderParameters/Indus.cpp
+++ OrderParameters/Indus.cpp
@@ -6,14 +6,52 @@ namespace OrderParametersRegistry
 }
 
 Indus::Indus(const OrderParametersInput& input)
-:OrderParameters(input)
+:OrderParameters(input), pv_(const_cast<ProbeVolumeRegistry&>(input.pv_registry_))
 {
-    std::string pv_name;
-    input.pack_.ReadString("probevolume", ParameterPack::KeyType::Required, pv_name);
-
-    auto& pv = input.pv_registry_.getProbeVolume(pv_name);
-    pv.setGeometry();
+    input.pack_.ReadString("probevolume", ParameterPack::KeyType::Required, pvName_);
+    input.pack_.ReadString("atomgroup", ParameterPack::KeyType::Required, atomGroupName_);
 
     registerOutput("n", [this](void)->Real {return this->getN();});
     registerOutput("ntilde", [this](void)->Real {return this->getNtilde();});
+}
+
+void Indus::calculate()
+{
+    auto& ag  = simstate_.getAtomGroup(atomGroupName_);
+    auto& pos = ag.getAtomPositions();
+    auto& pv  = pv_.getProbeVolume(pvName_);
+
+    #pragma omp parallel
+    {
+        Real local_N = 0;
+        Real local_Ntilde = 0.0;
+        #pragma omp for
+        for (int i=0;i<pos.size();i++)
+        { 
+            int globalIndices = ag.AtomGroupIndices2GlobalIndices(i); 
+            ProbeVolumeOutput output = pv.calculate(pos[i]);
+            
+            ASSERT((output.htilde_x_ <= 1 && output.htilde_x_ >= 0), "htilde_x_ is not less or equal to 1 and larger or equal to 0, it is equal to " << output.htilde_x_);
+
+            local_Ntilde += output.htilde_x_;
+            local_N      += output.hx_;
+        }
+
+        #pragma omp critical
+        {
+            Ntilde_ += local_Ntilde;
+            N_ += local_N;
+        }
+    }
+}
+
+void Indus::update()
+{
+    // set orderParameters to zero
+    N_ = 0;
+    Ntilde_ = 0;
+
+    // update the probeVolume as needed 
+    auto& ProbeV = pv_.accessProbeVolume(pvName_);
+    ProbeV.update();
 }
--- OrderParameters/Indus.h
+++ OrderParameters/Indus.h
@@ -1,5 +1,7 @@
 #include "OrderParameters.h"
 #include "ProbeVolume.h"
+#include "parallel/OpenMP_buffer.h"
+#include "ProbeVolumeRegistry.h"
 
 class Indus:public OrderParameters
 {
@@ -7,12 +9,18 @@ class Indus:public OrderParameters
         Indus(const OrderParametersInput& input);
         virtual ~Indus(){};
 
-        virtual void calculate() override{};
+        virtual void calculate() override;
+        virtual void update() override;
 
+        // getters
         Real getN() const {return N_;}
         Real getNtilde() const {return Ntilde_;}
     
     private:
         Real N_;
         Real Ntilde_;
+
+        std::string atomGroupName_;
+        std::string pvName_;
+        ProbeVolumeRegistry& pv_;
 };
