ee144df7bfb618772b474694afe162f1f6f4c6d4
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-01 22:22:06 -0400

    Added p2 as a orderparameter

---
+++ OrderParameters/P2.cpp
@@ -0,0 +1,88 @@
+#include "P2.h"
+
+namespace OrderParametersRegistry
+{
+    registry_<P2> registerP2("p2");
+}
+
+P2::P2(const OrderParametersInput& input)
+:OrderParameters(input)
+{
+    input.pack_.ReadString("tailgroup", ParameterPack::KeyType::Required, tailgroupname_);
+    input.pack_.ReadString("headgroup", ParameterPack::KeyType::Required, headgroupname_);
+    input.pack_.ReadString("name", ParameterPack::KeyType::Required, name_);
+
+    auto& tailAG = simstate_.getAtomGroup(tailgroupname_);
+    auto& headAG = simstate_.getAtomGroup(headgroupname_);
+
+    tailgroupsize_ = tailAG.getAtomGroupIndices().size();
+    headgroupsize_ = headAG.getAtomGroupIndices().size();
+
+    ASSERT((tailgroupsize_ == headgroupsize_), "The number of atoms passed in for headgroup is less than that of tail group.");
+
+    registerOutput("p2",[this](void)-> Real {return this->getP2();});
+    registerOutput("qxx", [this](void)->Real {return this->getQxx();});
+    registerOutput("qxy", [this](void)->Real {return this->getQxy();});
+    registerOutput("qxz", [this](void)->Real {return this ->getQxz();});
+    registerOutput("qyy", [this](void)->Real {return this->getQyy();});
+    registerOutput("qyz", [this](void) ->Real {return this->getQyz();});
+}
+
+void P2::calculate()
+{
+    // zero the Qtensor
+    Qtensor_.fill({});
+
+    auto& tailAG = simstate_.getAtomGroup(tailgroupname_);
+    auto& headAG = simstate_.getAtomGroup(headgroupname_);
+
+    auto& tailpositions_ = tailAG.getAtomPositions();
+    auto& headpositions_ = headAG.getAtomPositions();
+
+    #pragma omp parallel
+    {
+        Matrix Qtensor_local = {};
+        #pragma omp for
+        for (int i=0;i<tailpositions_.size();i++)
+        {
+            Real3 tailatompos_ = tailpositions_[i];
+            Real3 headatompos_ = headpositions_[i];
+
+            Real3 localdirector = {};
+            Real sq_dist = 0.0;
+            simbox_.calculateDistance(headatompos_, tailatompos_, localdirector, sq_dist);
+
+            // normalize the local director
+            localdirector = Qtensor::vec_mult(1.0/std::sqrt(sq_dist), localdirector);
+
+            // calculate the atomic Qtensor
+            Matrix Qtensor_atomic = Qtensor::vec_dyadic(localdirector, localdirector);
+            
+            Qtensor::matrix_mult_inplace(Qtensor_atomic, 3);
+            Qtensor_atomic = Qtensor::matrix_sub(Qtensor_atomic, Qtensor::matrix_Identity());
+
+            // accumulate the local Qtensor
+            Qtensor::matrix_accum_inplace(Qtensor_local, Qtensor_atomic);
+        }
+
+        #pragma omp critical
+        {
+            Qtensor::matrix_accum_inplace(Qtensor_, Qtensor_local);
+        }
+    }
+
+    Qtensor::matrix_mult_inplace(Qtensor_, 1.0/(2.0*tailgroupsize_));
+
+    // find the p2 variable as well as the eigenvalue
+    auto pair = Qtensor::OP_Qtensor(Qtensor_);
+    P2_OP_ = pair.first;
+    v1_    = pair.second;
+}
+
+void P2::update()
+{
+    P2_OP_ = 0;
+    v1_.fill(0);
+
+    Qtensor_.fill({});
+}
---
+++ OrderParameters/P2.h
@@ -0,0 +1,44 @@
+#include "OrderParameters.h"
+#include "tools/Assert.h"
+#include "Qtensor.h"
+#include "tools/CommonTypes.h"
+#include "parallel/OpenMP_buffer.h"
+
+#include <string>
+#include <vector>
+
+class P2: public OrderParameters
+{
+    public:
+        using Matrix = CommonTypes::Matrix;
+        using Real   = CommonTypes::Real;
+        using Real3  = CommonTypes::Real3;
+
+        P2(const OrderParametersInput& input);
+        virtual ~P2(){};
+        virtual void calculate() override;
+        virtual void update() override;
+
+        Real getP2() const {return P2_OP_;}
+        Real getv1x() const {return v1_[0];}
+        Real getv1y() const {return v1_[1];}
+        Real getv1z() const {return v1_[2];}
+        Real getQxx() const {return Qtensor_[0][0];}
+        Real getQxy() const {return Qtensor_[0][1];}
+        Real getQxz() const {return Qtensor_[0][2];}
+        Real getQyy() const {return Qtensor_[1][1];}
+        Real getQyz() const {return Qtensor_[1][2];}
+
+    private:
+        std::string tailgroupname_;
+        std::size_t tailgroupsize_;
+
+        std::string headgroupname_;
+        std::size_t headgroupsize_;
+
+        Real P2_OP_;
+        Real3 v1_;
+        Matrix Qtensor_;
+
+        OpenMP::OpenMP_buffer<Matrix> Qtensor_buffer_;
+};
