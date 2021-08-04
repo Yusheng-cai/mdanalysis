f3cb07564d2c44c55d235767f44576814b844d9a
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-04 15:56:45 -0400

    Added functionality in AtomDerivativesSet to insert with OMP enabled

--- OrderParameters/AtomDerivativesSet.cpp
+++ OrderParameters/AtomDerivativesSet.cpp
@@ -12,10 +12,63 @@ void AtomDerivativesSet::insert(int index, const Real3& derivatives)
     ad.index = index;
 
     AtomDerivatives_.push_back(ad);
+
+    Index_Map_.insert(std::make_pair(index, true));
 }
 
 void AtomDerivativesSet::clear()
 {
     AtomDerivatives_.clear();
     Index_Map_.clear();
+}
+
+void AtomDerivativesSet::insertOMP(int index, const Real3& derivatives)
+{ 
+    int numthreads = OpenMP::get_num_threads();
+
+    if (! OpenMP::in_parallel())
+    {
+        ASSERT((numthreads == 1), "You are trying to insert into AtomDerivativesSet with OMP but it is neither in a parallel region, nor is number of \
+        threads = 1.");
+
+        // if OMP threads = 1, then just perform norming insertion
+        insert(index, derivatives);
+    }
+    else
+    {
+        // access the atom derivatives by OMP id
+        auto& atomderivative = AtomDerivativesBuffer_.access_buffer_by_id();
+        
+        AtomDerivatives ad;
+
+        ad.derivatives = derivatives;
+        ad.index = index;
+
+        atomderivative.push_back(ad);
+    }
+}
+
+void AtomDerivativesSet::CombineAndClearOMPBuffer()
+{
+    int size = AtomDerivatives_.size();
+    for (auto it = AtomDerivativesBuffer_.beginworker(); it != AtomDerivativesBuffer_.endworker();it++)
+    {
+        size += it -> size();
+    }
+
+
+    AtomDerivatives_.reserve(size);
+
+    for (auto it = AtomDerivativesBuffer_.beginworker();it!=AtomDerivativesBuffer_.endworker();it++)
+    {
+        AtomDerivatives_.insert(AtomDerivatives_.end(), it->begin(), it ->end());
+    }
+
+    // clear the buffer
+    AtomDerivativesBuffer_.clearBuffer();
+}
+
+void AtomDerivativesSet::setMasterObject()
+{
+    AtomDerivativesBuffer_.set_master_object(AtomDerivatives_);
 }
--- OrderParameters/AtomDerivativesSet.h
+++ OrderParameters/AtomDerivativesSet.h
@@ -1,5 +1,6 @@
 #include "tools/Assert.h"
 #include "tools/CommonTypes.h"
+#include "parallel/OpenMP_buffer.h"
 
 #include <vector>
 #include <map>
@@ -29,13 +30,23 @@ class AtomDerivativesSet
         void insert(int index, const Real3& derivative);
         void clear();
 
+        std::size_t size() const {return AtomDerivatives_.size();}
+
         const std::vector<AtomDerivatives>& getAtomDerivatives() const {return AtomDerivatives_;}
+        const AtomDerivatives& getAtomDerivativeByIndex(int index) const {return AtomDerivatives_[index];} 
         std::vector<AtomDerivatives>& accessAtomDerivatives() {return AtomDerivatives_;};
         AtomDerivatives& accessAtomDerivativeByIndex(int index) {return AtomDerivatives_[index];}
+
+        void insertOMP(int index, const Real3& derivative);
+        void CombineAndClearOMPBuffer();
+        void setMasterObject();
 
 
     private:
         std::vector<AtomDerivatives> AtomDerivatives_;
+
+        // This is used solely for OMP regions
+        OpenMP::OpenMP_buffer<std::vector<AtomDerivatives>> AtomDerivativesBuffer_;
 
         // an index map that keeps track of all the atom indices inserted
         std::map<int, bool> Index_Map_;
