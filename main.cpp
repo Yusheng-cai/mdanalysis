092d2f2aba11c0b51837d03f4533c1d8cc21f078
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-05 16:09:36 -0400

    Changed a few things in main.cpp

--- main.cpp
+++ main.cpp
@@ -24,11 +24,17 @@ int main(int argc, char** argv)
 
     Driver d(fname,cmd);
 
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i=0;i<d.getNframes();i++)
     {
         d.update();
         d.calculate();
     }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::cout << "Entire operation took " << duration.count() << " microseconds"<< std::endl;
  
     return 0;
 };
