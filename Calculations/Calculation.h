6c8e7f394e1303b5f02f16d10a983e041704e7a9
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-24 23:47:20 -0400

    Minor changes to calculation base class

--- Calculations/Calculation.h
+++ Calculations/Calculation.h
@@ -1,6 +1,9 @@
 #include "tools/Assert.h"
 #include "tools/GenericFactory.h"
 #include "tools/InputParser.h"
+#include "OrderParameters/SimulationState.h"
+#include "xdr/TopologyReader.h"
+#include "xdr/GroFile.h"
 
 #include <vector>
 #include <array>
@@ -10,17 +13,23 @@
 struct CalculationInput
 {
     ParameterPack& pack_;
+    SimulationState& simstate_;
+    TopologyReader& top;
+    GroFile& gro;
 };
 
 class Calculation
 {
     public:
-        Calculation(const CalculationInput& input);
+        Calculation(const CalculationInput& input):top_(input.top), gro_(input.gro), simstate_(input.simstate_){};
 
         virtual void update(){};
         virtual void calculate() = 0;
 
     protected:
+        TopologyReader& top_;
+        GroFile& gro_;
+        SimulationState& simstate_;
 };
 
 namespace CalculationRegistry
