7957731ac787f9cafd81d1b50890d6b08523a4da
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-24 23:49:12 -0400

    Added xdr tester code

---
+++ test/testXdr/main.cpp
@@ -0,0 +1,17 @@
+#include "xdr/TopologyReader.h"
+#include "tools/Assert.h"
+#include "tools/CommonTypes.h"
+
+#include <string>
+#include <vector>
+#include <array>
+
+int main(int argc, char** argv)
+{
+    std::string fname(argv[1]);
+
+    TopologyReader top;
+    top.Parse(fname);
+
+    top.print();
+}
