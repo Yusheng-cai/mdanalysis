883f283a28e7875c9666cd33e791f2ac3cc9576a
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-26 10:02:43 -0400

    Added function to qtensorZ to bin the COM and perform calculations

--- OrderParameters/QtensorZ.cpp
+++ OrderParameters/QtensorZ.cpp
@@ -1,11 +1,31 @@
 #include "QtensorZ.h"
 
+namespace CalculationRegistry
+{
+    registry_<QtensorZ> registerQtensorZ("qtensorZ");
+}
+
 QtensorZ::QtensorZ(const CalculationInput& input)
 :Calculation(input)
 {
     input.pack_.ReadString("residuegroup", ParameterPack::KeyType::Required, residueName_);
     input.pack_.ReadString("direction", ParameterPack::KeyType::Optional, direction_);
-    input.pack_.ReadArrayNumber("range", ParameterPack::KeyType::Required, range_);
+    input.pack_.ReadNumber("headindex", ParameterPack::KeyType::Required, headIndex_);
+    input.pack_.ReadNumber("tailindex", ParameterPack::KeyType::Required, tailIndex_);
+    input.pack_.ReadVectorNumber("comIndices", ParameterPack::KeyType::Optional, COMIndices_);
+
+    headIndex_ --;
+    tailIndex_ --;
+
+    for (int i=0;i<COMIndices_.size();i++)
+    {
+        COMIndices_[i] -= 1;
+    }
+
+    auto binPack = input.pack_.findParamPack("bin", ParameterPack::KeyType::Required);
+
+    // create the bin object
+    bin_ = Binptr(new Bin(*binPack));
 
     auto it = MapNameToDirection.find(direction_);
     ASSERT((it != MapNameToDirection.end()), "The direction " << direction_ << " is not recognized.");
@@ -13,13 +33,84 @@ QtensorZ::QtensorZ(const CalculationInput& input)
 
     // add the residue group to the system
     addResidueGroup(residueName_);
+
+    // resize the binned matrix to number of bins
+    BinnedMatrix_.resize(bin_->getNumbins());
+
+    // resize the P2 to be number of bins size
+    P2_.resize(bin_->getNumbins());
+    std::fill(P2_.begin(), P2_.end(), 0.0);
 }
 
 void QtensorZ::calculate()
 {
     // obtain the residue group by its name
-    const auto& res = getResidueGroup(residueName_);
+    const auto& res = getResidueGroup(residueName_).getResidues();
 
     // obtain the COM
-    std::vector<Real3> COM = CalculationTools::getCOM(res.getResidues(),simstate_);
+    std::vector<Real3> COM = CalculationTools::getCOM(res,simstate_);
+
+    // make all the matrix in vector zero
+    Matrix zeroMatrix;
+    zeroMatrix.fill({});
+    std::fill(BinnedMatrix_.begin(), BinnedMatrix_.end(), zeroMatrix);
+
+    // for (int i=0;i<COM.size();i++)
+    // {
+    //     for (int j=0;j<3;j++)
+    //     {
+    //         std::cout << COM[i][j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+
+    std::vector<int> NumResPerBin_;
+    NumResPerBin_.resize(bin_->getNumbins());
+    std::fill(NumResPerBin_.begin(), NumResPerBin_.end(),0);
+
+    // Bin the COMs
+    for (int i=0;i<COM.size();i++)
+    {
+        Real num = COM[i][index_];
+
+        int binNum = bin_ -> findBin(num);
+
+        Real3 headPos = res[i].atoms_[headIndex_].positions_;
+        Real3 tailPos = res[i].atoms_[tailIndex_].positions_;
+
+        Real3 diff;
+        Real diff_sq;
+
+        simstate_.getSimulationBox().calculateDistance(headPos, tailPos, diff, diff_sq);
+
+        // normalize the director and calculate the dyad
+        Qtensor::normalize_director(diff);
+        Matrix dyad = Qtensor::vec_dyadic(diff, diff);
+
+        Qtensor::matrix_mult_inplace(dyad, 3.0);
+        Matrix Qlocal = Qtensor::matrix_sub(dyad, Qtensor::matrix_Identity());
+
+        Qtensor::matrix_accum_inplace(BinnedMatrix_[binNum], Qlocal);
+
+        NumResPerBin_[binNum] += 1;
+    }
+
+    for (int i=0;i<BinnedMatrix_.size();i++)
+    {
+        Qtensor::matrix_mult_inplace(BinnedMatrix_[i], 1.0/(2.0*NumResPerBin_[i]));
+
+        auto result = Qtensor::OP_Qtensor(BinnedMatrix_[i]);
+
+        P2_[i] += result.first;
+    }
+}
+
+void QtensorZ::finishCalculate()
+{
+    int totalFrames = simstate_.getTotalFrames();
+
+    for (int i=0;i<P2_.size();i++)
+    {
+        P2_[i] /= totalFrames;
+    }
 }
--- OrderParameters/QtensorZ.h
+++ OrderParameters/QtensorZ.h
@@ -1,11 +1,16 @@
+#pragma once
 #include "Calculation.h"
 #include "tools/Assert.h"
 #include "xdr/MoleculeStructs.h"
 #include "CalculationTools.h"
+#include "tools/CommonTypes.h"
+#include "Bin.h"
+#include "Qtensor.h"
 
 #include <vector>
 #include <array>
 #include <string>
+#include <memory>
 
 // Class that calculates and average Qtensor as a function of z over time
 // This can give P2 as a function of z(z can be any dimension)
@@ -15,11 +20,16 @@ class QtensorZ : public Calculation
 {
     public:
         using Range = CommonTypes::Real2;
+        using Binptr= std::unique_ptr<Bin>;
+        using Matrix= CommonTypes::Matrix;
 
         QtensorZ(const CalculationInput& input);
         virtual ~QtensorZ(){};
 
         virtual void calculate() override;
+        virtual void finishCalculate() override;
+
+        std::vector<Real>& getP2() {return P2_;}
 
     private:
         std::string residueName_;
@@ -30,7 +40,7 @@ class QtensorZ : public Calculation
         // Get the Qtensor for each of the bins
 
         // The string that indicates the direction
-        std::string direction_;
+        std::string direction_ = "z";
         int index_;
 
         // Map direction string to index
@@ -41,5 +51,16 @@ class QtensorZ : public Calculation
             {"z", 2}
         };
 
-        Range range_;
+        Binptr bin_;
+
+        std::vector<Matrix> BinnedMatrix_;
+
+        // The head and tail index within the 5CB molecule
+        int headIndex_;
+        int tailIndex_;
+
+        // The indices of the atoms where COM is calculated wrt 
+        std::vector<int> COMIndices_;
+
+        std::vector<Real> P2_;
 };
