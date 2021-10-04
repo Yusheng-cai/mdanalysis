9abb06c76c2ca09ec4fc21db91a63341eaf6f893
Yusheng Cai <cys9741@seas.upenn.edu>
2021-10-04 17:34:44 -0400

    minor changes

--- OrderParameters/P2.cpp
+++ OrderParameters/P2.cpp
@@ -9,11 +9,17 @@ P2::P2(const OrderParametersInput& input)
 :liquid_crystal(input)
 {
     registerOutput("p2",[this](void)-> Real {return this->getP2();});
+    registerOutput("p20", [this](void) -> Real {return this->getP20();});
+    registerOutput("p21", [this](void) -> Real {return this->getP21();});
+    registerOutput("p22", [this](void) -> Real {return this->getP22();});
     registerOutput("qxx", [this](void)->Real {return this->getQxx();});
     registerOutput("qxy", [this](void)->Real {return this->getQxy();});
     registerOutput("qxz", [this](void)->Real {return this ->getQxz();});
     registerOutput("qyy", [this](void)->Real {return this->getQyy();});
     registerOutput("qyz", [this](void) ->Real {return this->getQyz();});
+    registerOutput("v0x", [this](void) -> Real {return this -> getv0x();});
+    registerOutput("v0y", [this](void) -> Real {return this -> getv0y();});
+    registerOutput("v0z", [this](void) -> Real {return this -> getv0z();});
 }
 
 void P2::calculate()
@@ -23,15 +29,26 @@ void P2::calculate()
     calcQtensor();
 
     // find the p2 variable as well as the eigenvalue
-    auto pair = Qtensor::OP_Qtensor(Qtensor_);
-    P2_OP_ = pair.first;
-    v1_    = pair.second;
+    auto orderedEigPair = Qtensor::orderedeig_Qtensor(Qtensor_);
 
-    auto headatomDerivatives_ = accessDerivatives(headgroupname_);
-    auto tailatomDerivatives_ = accessDerivatives(tailgroupname_);
+    P2_OP_ = -2.0*orderedEigPair.second[1];
+    for (int i=0;i<3;i++)
+    {
+        v0_[i]    = orderedEigPair.first[i][0];
+        v1_[i]    = orderedEigPair.first[i][1];
+        v2_[i]    = orderedEigPair.first[i][2];
+    }
 
-    // clear all the derivatives stored in derivativesOutputs
-    clearDerivativesOutputs();
+    // calculate p2 in each of the directions of the eigenvectors 
+    p2_0_ = 0.0;
+    p2_1_ = 0.0;
+    p2_2_ = 0.0;
+    for (int i=0;i<uij_.size();i++)
+    {
+        p2_0_ += 3*std::pow(Qtensor::vec_dot(uij_[i], v0_),2.0) - 1;
+        p2_1_ += 3*std::pow(Qtensor::vec_dot(uij_[i], v1_),2.0) - 1;
+        p2_2_ += 3*std::pow(Qtensor::vec_dot(uij_[i], v2_),2.0) - 1;
+    }
 
     auto& headatomgroup_ = getAtomGroup(headgroupname_);
     auto& tailatomgroup_ = getAtomGroup(tailgroupname_);
@@ -40,6 +57,16 @@ void P2::calculate()
     auto& tailatoms_ = tailatomgroup_.getAtoms();
 
     Real N = tailgroupsize_;
+
+    p2_0_ /= (2.0*N); 
+    p2_1_ /= (2.0*N);
+    p2_2_ /= (2.0*N);
+
+    auto headatomDerivatives_ = accessDerivatives(headgroupname_);
+    auto tailatomDerivatives_ = accessDerivatives(tailgroupname_);
+
+    // clear all the derivatives stored in derivativesOutputs
+    clearDerivativesOutputs();
 
     #pragma omp parallel
     {
