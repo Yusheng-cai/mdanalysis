c58e33d0321e464ae66e9cc8f5e412461343573f
Yusheng Cai <cys9741@seas.upenn.edu>
2021-10-04 17:34:13 -0400

    Added new OP

---
+++ OrderParameters/Pcost2Qtensor.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Calculation.h"
+#include "Bin.h"
+#include "CalculationTools.h"
+#include "Qtensor.h"
+#include "Pcost.h"
+
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <memory>
+#include <map>
+#include <iostream>
+#include <iomanip>
+
+// calculate distribution of cosine theta of the angles in a probe volume
+
+class Pcost2Qtensor : public Calculation
+{
+    public:
+        using Matrix = CommonTypes::Matrix;
+        using Real3  = CommonTypes::Real3;
+        using Real   = CommonTypes::Real;
+
+        Pcost2Qtensor(const CalculationInput& input);
+
+        void printcost20PerResIter(std::ofstream& ofs);
+        void printcost21PerResIter(std::ofstream& ofs);
+        void printcost22PerResIter(std::ofstream& ofs);
+        virtual void calculate() override;
+        virtual void finishCalculate() override {};
+    
+    private:
+        Matrix Qtensor_;
+
+        // The director of each of the molecules
+        std::vector<Real3> uij_;
+        std::vector<Real> norm_;
+        std::vector<std::vector<Real>> cost2Peratom_;
+        std::vector<std::vector<Real>> cost2PerRes_;
+
+        // The eigenvector directions 
+        Matrix eigenvector_;
+        Real3 v0_;
+        Real3 v1_;
+        Real3 v2_;
+
+        int head_index_;
+        int tail_index_;
+
+        std::string residue_name_;
+};
