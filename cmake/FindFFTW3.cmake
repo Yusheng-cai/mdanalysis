9b256ad4c9e49b429e821db4b07bdc3ea18db8ae
Yusheng Cai <cys9741@seas.upenn.edu>
2021-10-11 10:40:59 -0400

    Added scripts to find fftw3

---
+++ cmake/FindFFTW3.cmake
@@ -0,0 +1,38 @@
+# A cmake script that searches for FFTW3 library 
+# If we were to include this directory (cmake), when we do 
+# FIND_PACKAGE(foo), it will search for the file Findfoo.cmake
+
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+# Checks the environmental variable
+if(FFTW3_DIR)
+	# Use the hint provided
+	list(APPEND CMAKE_PREFIX_PATH ${FFTW3_DIR})
+endif() 
+
+find_path(
+    FFTW3_INCLUDE_DIR 
+        fftw3.h
+    HINTS
+        ${FFTW3_DIR}
+)
+
+find_library(
+    FFTW3_LIBRARY
+	NAMES fftw3f
+    PATH_SUFFIXES "a"
+	HINTS ${FFTW3_DIR}
+)
+
+
+find_package_handle_standard_args( FFTW3 DEFAULT_MSG
+  FFTW3_INCLUDE_DIR
+  FFTW3_LIBRARY
+)
+
+if (FFTW3_FOUND)
+    SET( FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR} )
+    SET( FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
+    message("Found FFTW3. FFTW3_LIBRARY: ${FFTW3_LIBRARIES}. FFTW3_INCLUDE_DIRS: ${FFTW3_INCLUDE_DIRS}.")
+endif()
