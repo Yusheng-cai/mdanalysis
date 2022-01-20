1e5198523a3310f6cd1cc18c06428989d0ba9d97
Yusheng Cai <cys9741@seas.upenn.edu>
2022-01-19 23:19:36 -0500

    Added code for calculating cell grid for cell lists

---
+++ OrderParameters/CellGrid.cpp
@@ -0,0 +1,95 @@
+#include "CellGrid.h"
+
+CellGrid::CellGrid(SimulationState& simstate, Real dL, int searchnum)
+: simstate_(simstate), dL_(dL), searchnum_(searchnum)
+{
+    for (int i=-searchnum_;i<=searchnum_;i++)
+    {
+        for (int j=-searchnum_;j<=searchnum_;j++)
+        {
+            for (int k=-searchnum_;k<=searchnum_;k++)
+            {
+                index3 ind = {{i,j,k}};
+                Offsets_.push_back(ind);
+            }
+        }
+    }
+}
+
+void CellGrid::update()
+{
+    Real3 Sides = simstate_.getSimulationBox().getSides();
+    // Example like 5 / 3 --> 1 but N should be 2  -- > 0 1
+    for (int i=0;i<3;i++)
+    {
+        N_[i] = (int)std::floor(Sides[i]/dL_) + 1;
+    }
+}
+
+int CellGrid::ConvertGridIndexToIndex(index3& index)
+{
+    // convert index into x + y * Nx + z * Nx * Ny
+    int sum = index[0] + index[1] * N_[0] + index[2] * N_[1] * N_[0];
+
+    return sum;
+}
+
+CellGrid::index3 CellGrid::getCellGridIndex(const Real3& pos)
+{
+    // first let's shift the position into the simulation box
+    Real3 shiftedPos = simstate_.getSimulationBox().shiftIntoBox(pos);
+
+    // Then let's put the shifted Pos into a cell grid
+    index3 index;
+
+    for (int i=0;i<3;i++)
+    {
+        index[i] = (int)std::floor(shiftedPos[i] / dL_ );
+    }
+
+    return index;
+}
+
+int CellGrid::getCellGridIntIndex(const Real3& pos)
+{
+    index3 gridIndex = getCellGridIndex(pos);
+    
+    return ConvertGridIndexToIndex(gridIndex);
+}
+
+CellGrid::index3 CellGrid::FixIndex(index3& index)
+{
+    index3 ret;
+
+    for (int i=0;i<3;i++)
+    {
+        if (index[i] < 0)
+        {
+            index[i] += N_[i];
+        }
+
+        ret[i] = index[i] % N_[i];
+    }
+
+    return ret;
+}
+
+std::vector<int> CellGrid::getNeighborIndex(const Real3& pos)
+{
+    index3 index = getCellGridIndex(pos);
+    std::vector<int> neighborIndex;
+
+    for (auto& off : Offsets_)
+    {
+        index3 newIndex = {};
+        for (int i=0;i<3;i++)
+        {
+            newIndex[i] = index[i] + off[i];
+        }  
+        newIndex = FixIndex(newIndex);
+        int ind  = ConvertGridIndexToIndex(newIndex);
+        neighborIndex.push_back(ind);
+    }
+
+    return neighborIndex;
+}
---
+++ OrderParameters/CellGrid.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "tools/Assert.h"
+#include "tools/CommonTypes.h"
+#include "SimulationState.h"
+
+#include <vector>
+#include <array>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class CellGrid
+{
+    public:
+        using Real = CommonTypes::Real;
+        using Real3= CommonTypes::Real3;
+        using index3= CommonTypes::index3;
+
+        CellGrid(SimulationState& simstate, Real dL, int searchnum);
+
+        void update();
+
+        index3 getCellGridIndex(const Real3& pos);
+        int ConvertGridIndexToIndex(index3& index);
+        int getCellGridIntIndex(const Real3& pos);
+        std::vector<int> getNeighborIndex(const Real3& pos);
+        index3 FixIndex(index3& index);
+        int getSize() const {return N_[0] * N_[1] * N_[2];}
+
+    private:
+        Real dL_;
+        Real3 L_;
+        SimulationState& simstate_;
+        index3 N_;
+        int searchnum_;
+
+        std::vector<index3> Offsets_;
+};
