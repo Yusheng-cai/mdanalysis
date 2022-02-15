57dd54d8f9b3c1a7312456bc89d9aa8e55f82782
Yusheng Cai <cys9741@seas.upenn.edu>
2022-02-15 10:24:06 -0500

    Added short ranged electrostatics as an OP

---
+++ OrderParameters/SRE.cpp
@@ -0,0 +1,127 @@
+#include "SRE.h"
+
+namespace CalculationRegistry
+{
+    registry_<SRE> registerSRE("SRE");
+}
+
+SRE::SRE(const CalculationInput& input) 
+:Calculation(input)
+{
+    // This could be LC or water
+    pack_.ReadString("solvent", ParameterPack::KeyType::Required, SolventName_);
+    // This is usually SAM 
+    pack_.ReadString("solute", ParameterPack::KeyType::Required, SoluteName_);
+    // Read in the alpha parameter
+    pack_.ReadNumber("epsilon", ParameterPack::KeyType::Required, epsilon_);
+    // read in the cutoff
+    pack_.ReadNumber("cutoff", ParameterPack::KeyType::Optional, cutoff_);
+    alpha_ = 1.0 / epsilon_;
+
+    addResidueGroup(SolventName_);
+    addResidueGroup(SoluteName_);
+
+    // register the printing function for energy 
+    registerPerIterOutputFunction("energy", [this](std::ofstream& ofs) -> void {this -> printEnergyPerIter(ofs);});
+
+    cell_ = Cellptr(new CellGrid(simstate_, epsilon_,3));
+}
+
+void SRE::update()
+{
+    cell_ -> update();
+}
+
+void SRE::calculate()
+{
+    const auto& solvent = getResidueGroup(SolventName_).getTotalResidue().atoms_;
+    const auto& solute  = getResidueGroup(SoluteName_).getTotalResidue().atoms_;
+
+    // make all solute into cellgrid
+    int num = cell_->getSize();
+    std::vector<std::vector<int>> celllist_(num);
+    for (int i=0;i<solute.size();i++)
+    {
+        Real3 pos = solute[i].positions_;
+        index3 id = cell_ -> getCellGridIndex(pos);
+        int ind  = cell_ -> getCellGridIntIndex(pos);
+        ASSERT((ind >= 0 && ind < num), "Index out of range, max is " << num << " while it is " << ind);
+
+        celllist_[ind].push_back(i);
+    }
+
+    // iterate over all the atoms in solvent and solute
+    energy_ = 0.0;
+    #pragma omp parallel
+    {
+        Real sum = 0.0;
+        #pragma omp for
+        for (int i=0;i<solvent.size();i++)
+        {
+            Real3 possv = solvent[i].positions_;
+            Real qi     = solvent[i].charge_;
+            std::vector<int> Neighbors = cell_ -> getNeighborIndex(possv);
+
+            for (int ind : Neighbors)
+            {
+                ASSERT((ind < num), "Ind is " << ind << " max is " << num);
+                for (int index : celllist_[ind])
+                {
+                    Real3 possl = solute[index].positions_;
+                    Real qj     = solute[index].charge_;
+                    Real3 dist;
+                    Real distsq;
+
+                    simstate_.getSimulationBox().calculateDistance(possv, possl, dist, distsq);
+
+                    Real r = std::sqrt(distsq);
+
+                    sum += factor_ * qi * qj * std::erfc(r * alpha_) / r;
+                }
+            }
+        }
+
+        #pragma omp critical
+        {
+            energy_ += sum;
+        }
+    }
+
+    // energy_ = 0.0;
+    // #pragma omp parallel
+    // {
+    //     Real sum = 0.0;
+    //     #pragma omp for
+    //     for (int i=0;i<solvent.size();i++)
+    //     {
+    //         Real3 possv = solvent[i].positions_;
+    //         Real qi     = solvent[i].charge_;
+
+    //         for (int j=0;j<solute.size();j++)
+    //         {
+    //             Real3 possl = solute[j].positions_;
+    //             Real qj     = solute[j].charge_;
+    //             Real3 dist;
+    //             Real distsq;
+
+    //             simstate_.getSimulationBox().calculateDistance(possv, possl, dist, distsq);
+
+    //             Real r = std::sqrt(distsq);
+
+    //             sum += factor_ * qi * qj * std::erfc(r * alpha_)  / r;
+    //         }
+    //     }
+
+    //     #pragma omp critical
+    //     {
+    //         energy_ += sum;
+    //     }
+    // }
+
+    std::cout << "Energy = " << energy_ << "\n";
+}
+
+void SRE::printEnergyPerIter(std::ofstream& ofs)
+{
+    ofs << energy_ << "\n";
+}
---
+++ OrderParameters/SRE.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Calculation.h"
+#include "tools/Assert.h"
+#include "tools/CommonTypes.h"
+#include "SimulationState.h"
+#include "CellGrid.h"
+
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <string>
+#include <array>
+
+class SRE : public Calculation
+{
+    public:
+        using index3 = CommonTypes::index3;
+        using Cellptr= std::unique_ptr<CellGrid>;
+
+        SRE(const CalculationInput& input);
+
+        virtual void calculate() override;
+        virtual void finishCalculate() override {};
+        virtual void update() override;
+
+        void printEnergyPerIter(std::ofstream& ofs);
+
+    private:
+        std::string SolventName_;
+        std::string SoluteName_;
+        Real epsilon_;
+        Real alpha_;
+        Real cutoff_=1.2;
+
+        // 1/(4 pi epsilon) as defined by gromac --> kJ nm /(mol e2) 
+        Real factor_ = 138.935458;
+
+        Real energy_=0.0;
+
+        Cellptr cell_;
+};
