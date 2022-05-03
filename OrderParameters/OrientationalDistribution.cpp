6980b78f6316193bdd25468ea1d4261a89f71a6a
Yusheng Cai <cys9741@seas.upenn.edu>
2022-05-03 16:53:36 -0400

    Updated Orientational Distribution

---
+++ OrderParameters/OrientationalDistribution.cpp
@@ -0,0 +1,114 @@
+#include "OrientationalDistribution.h"
+
+namespace CalculationRegistry
+{
+    registry_<OrientationalDistribution> registerOrientationalDistribution("OrientationalDistribution");
+}
+
+OrientationalDistribution::OrientationalDistribution(const CalculationInput& input)
+:Calculation(input)
+{
+    pack_.ReadString("residue", ParameterPack::KeyType::Required, ResidueGroupName_);
+    pack_.ReadNumber("headindex", ParameterPack::KeyType::Required,HeadIndex_);
+    pack_.ReadNumber("tailindex", ParameterPack::KeyType::Required,TailIndex_);
+    pack_.ReadNumber("numbins", ParameterPack::KeyType::Required, NumBins_);
+    pack_.ReadArrayNumber("array", ParameterPack::KeyType::Optional, arr_);
+    HeadIndex_--;
+    TailIndex_--;
+
+    // initialize Residue
+    initializeResidueGroup(ResidueGroupName_);
+
+    // initialize the probe volumes
+    initializeProbeVolumes();
+    initializeNotInProbeVolumes();
+
+    // initialize the bins 
+    CosThetaBin_ = Binptr(new Bin(CosThetaRange_, NumBins_));
+    CosThetaSquaredBin_ = Binptr(new Bin(CosThetaSquaredRange_, NumBins_));
+
+    // register the output functions 
+    registerOutputs();
+}
+
+void OrientationalDistribution::registerOutputs()
+{
+    registerOutputFunction("Distribution", [this](std::string name) -> void {this -> PrintDistribution(name);});
+}
+
+void OrientationalDistribution::finishCalculate()
+{
+    int numFrames = simstate_.getTotalFrames();
+    Real CosThetaTot = 0.0;
+    Real CosSquaredThetaTot = 0.0;
+
+    for (int i=0;i<NumBins_;i++)
+    {
+        PCosTheta_[i] = PCosTheta_[i]/numFrames;
+        PCosThetaSquared_[i] = PCosThetaSquared_[i] / numFrames;
+
+        CosThetaTot += PCosTheta_[i];
+        CosSquaredThetaTot += PCosThetaSquared_[i];
+    }
+
+    for (int i=0;i<NumBins_;i++)
+    {
+        PCosTheta_[i] = PCosTheta_[i] / CosThetaTot;
+        PCosThetaSquared_[i] = PCosThetaSquared_[i] / CosSquaredThetaTot;
+    }
+}
+
+void OrientationalDistribution::PrintDistribution(std::string name)
+{
+    std::ofstream ofs;
+    ofs.open(name);
+
+    ofs << "# Bin\tcostheta\tcos2theta\tP(costheta)\tP(cos2theta)\n";
+
+    for (int i=0;i<NumBins_;i++)
+    {
+        ofs <<CosThetaBin_->getCenterLocationOfBin(i) << " " << CosThetaSquaredBin_->getCenterLocationOfBin(i) << " " \
+        << PCosTheta_[i] << " " << PCosThetaSquared_[i] << "\n";
+    }
+    ofs.close();
+}
+
+void OrientationalDistribution::calculate()
+{
+    auto& res = getResidueGroup(ResidueGroupName_).getResidues();
+    COM_.clear();
+    COM_.resize(res.size());
+    uij_.clear();
+    uij_.resize(res.size());
+
+    for (int i=0;i<res.size();i++)
+    {
+        COM_[i] = calcCOM(res[i]);
+
+        Real3 HeadPos = res[i].atoms_[HeadIndex_].positions_;
+        Real3 TailPos = res[i].atoms_[TailIndex_].positions_;
+        Real3 distance;
+        Real dist_sq;
+
+        simstate_.getSimulationBox().calculateDistance(HeadPos, TailPos,distance, dist_sq);
+        LinAlg3x3::normalize(distance);
+
+        uij_[i] = distance;
+    }
+
+    // find atoms within probe volume
+    std::vector<int> AtomIndices = InsidePVIndices(COM_);
+
+    // iterate over the atom indices
+    for (int i=0;i<AtomIndices.size();i++)
+    {
+        int k = AtomIndices[i];
+        Real cost = LinAlg3x3::DotProduct(uij_[k], arr_);
+
+        int CosThetaBinNum = CosThetaBin_->findBin(cost);
+        int CosThetaSquaredBinNum = CosThetaSquaredBin_->findBin(cost*cost);
+
+        PCosTheta_[CosThetaBinNum] += 1;
+        PCosThetaSquared_[CosThetaSquaredBinNum] += 1;
+    }
+}
---
+++ OrderParameters/OrientationalDistribution.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "Calculation.h"
+#include "Bin.h"
+#include "tools/CommonTypes.h"
+#include "LinAlgTools.h"
+#include "SimulationState.h"
+#include "ResidueGroup.h"
+
+#include <vector>
+#include <array>
+#include <string>
+#include <memory>
+
+/*
+    Class that finds the distribution of LC molecules within a certain region of interest (probe volume)
+*/
+class OrientationalDistribution : public Calculation
+{
+    public:
+        using Binptr  = std::unique_ptr<Bin>;
+        using Range   = std::array<Real,2>;
+
+        OrientationalDistribution(const CalculationInput& input);
+
+        void registerOutputs();
+
+        virtual ~OrientationalDistribution(){};
+        virtual void calculate();
+        virtual void update() {};
+        virtual void finishCalculate() override;
+
+        // printing functions
+        void PrintDistribution(std::string name);
+
+    private:
+        std::vector<Real> PCosTheta_;
+        std::vector<Real> PCosThetaSquared_;
+
+        // head and tail index used for identifying orientation of an LC molecule 
+        int HeadIndex_=1;
+        int TailIndex_=2;
+
+        // name of the residue group
+        std::string ResidueGroupName_;
+
+        // number of bins
+        int NumBins_;
+
+        // bins 
+        Binptr CosThetaBin_;
+        Range  CosThetaRange_ = {{-1.0,1.0}};
+        Binptr CosThetaSquaredBin_;
+        Range CosThetaSquaredRange_ = {{0.0,1.0}};
+
+        // director of each of the molecules
+        std::vector<Real3> uij_;
+
+        // array that we want to calculate cos theta on
+        Real3 arr_={{0,0,1}};
+};
