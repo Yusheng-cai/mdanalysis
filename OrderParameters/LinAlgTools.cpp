45da458b343b4366fa73b171029b4453c6f4ed1c
Yusheng Cai <cys9741@seas.upenn.edu>
2022-05-03 16:56:01 -0400

    Updated LinAlg Tools

--- OrderParameters/LinAlgTools.cpp
+++ OrderParameters/LinAlgTools.cpp
@@ -157,3 +157,263 @@ LinAlg3x3::Matrix LinAlg3x3::GetRotationMatrix(const Real3& v1, const Real3& v2)
 
     return ret;
 }
+
+LinAlg3x3::Real3 LinAlg3x3::vec_add(const LinAlg3x3::Real3& v1, const LinAlg3x3::Real3& v2)
+{
+  // Function that sum a vector with another vector
+  Real3 eval;
+  for(int i = 0; i < N_DIM; i++){
+    eval[i] = v1[i]+v2[i];
+  }
+  return eval;
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_add(const Real3& v1, const Real s){
+	Real3 eval;
+	for(int i=0;i<3;i++){
+		eval[i] = v1[i] + s;
+	}
+
+	return eval;
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_add(const Real s, const Real3& v1){
+	return LinAlg3x3::vec_add(v1,s);
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_sub(const Real3& v1, const Real3& v2)
+{ 
+  Real3 eval;
+  for(int i = 0; i < N_DIM; i++){
+    eval[i] = v1[i]-v2[i];
+  }
+  return eval;
+}
+
+
+LinAlg3x3::Real3 LinAlg3x3::vec_mult(const Real3& v1, const Real3& v2)
+{
+  Real3 eval;
+  for(int i = 0; i < N_DIM; i++){
+    eval[i] = v1[i]*v2[i];
+  }
+  return eval;
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_mult(const Real3& v1, const Real scalar_value)
+{
+  Real3 eval;
+  for(int i = 0; i < N_DIM; i++){
+    eval[i] = v1[i]*scalar_value;
+  }
+  return eval;
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_mult(const Real scalar_value, const Real3& v1){
+	return LinAlg3x3::vec_mult(v1,scalar_value);
+}
+
+LinAlg3x3::Real LinAlg3x3::vec_dot(const Real3& v1, const Real3& v2){
+	Real dot_product = 0.0;
+	for(int i=0;i<3;i++){
+		dot_product += v1[i]*v2[i];
+	}
+
+	return dot_product;
+}
+
+LinAlg3x3::Real LinAlg3x3::vec_norm(Real3 v1)
+{
+  Real eval=LinAlg3x3::vec_dot(v1,v1);
+
+  return std::sqrt(eval);
+}
+
+LinAlg3x3::Matrix LinAlg3x3::vec_dyadic(const Real3& v1, const Real3& v2)
+{
+  Matrix eval;
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+    eval[i][j] = v1[i]*v2[j];
+    }
+  }
+  return eval;
+}
+
+LinAlg3x3::Real3 LinAlg3x3::vec_cross(const Real3& v1, const Real3& v2){
+	Real3 output;
+	output.fill(0);
+
+	output[0] 	= v1[1]*v2[2] - v1[2]*v2[1];	
+	output[1]   = v1[2]*v2[0] - v1[0]*v2[2];
+	output[2]   = v1[0]*v2[1] - v1[1]*v2[0];  
+
+	return output;
+}
+
+LinAlg3x3::Real LinAlg3x3::matrix_trace(const Matrix& Q){
+	// Function that returns the trace of a 3x3 matrix (Q tensor)
+	// 
+	// Args:
+	// Q (LinAlg3x3::Matrix): 3x3 matrix of the Q tensor
+	//
+	// returns:
+	// trace (LinAlg3x3::Real): the trace of the Q tensor
+	
+	
+	Real trace = 0.0;
+	for(int i=0;i<3;i++){
+		trace = trace + Q[i][i]; 
+	}
+	return trace;
+}
+
+LinAlg3x3::Matrix LinAlg3x3::matrix_add(const Matrix& m1, const Matrix& m2)
+{
+  Matrix eval;
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+      eval[i][j] = m1[i][j]+m2[i][j];
+    }
+  }
+  return eval;
+}
+
+void LinAlg3x3::matrix_accum_inplace(Matrix& m1, const Matrix& m2)
+{
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+      m1[i][j] = m1[i][j]+m2[i][j];
+    }
+  }
+  return;
+}
+LinAlg3x3::Matrix LinAlg3x3::matrix_sub(const Matrix& m1, const Matrix& m2)
+{
+  // Function that subtracts a matrix from another matrix
+  Matrix eval;
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+      eval[i][j] = m1[i][j]-m2[i][j];
+    }
+  }
+  return eval;
+}
+
+LinAlg3x3::Matrix LinAlg3x3::matrix_Identity()
+{
+  // Function that returns the identity matrix
+  Matrix eval;
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+      if(i == j) eval[i][j] = 1.0;
+      else eval[i][j] = 0.0;
+    }
+  }
+  return eval;
+}
+
+void LinAlg3x3::matrix_mult_inplace(Matrix& m1, Real scalar)
+{
+  // Function that multiplies a matrix by a scalar in place (modifies the matrix in place)
+  for(int i = 0; i < N_DIM; i++){
+    for(int j = 0; j < N_DIM; j++){
+      m1[i][j] = m1[i][j]*scalar;
+    }
+  }
+  return;
+}
+
+void LinAlg3x3::matrix_zero_inplace(Matrix& m1)
+{
+	// Function that zeros out a matrix
+	m1.fill({});
+}
+
+LinAlg3x3::Matrix LinAlg3x3::matrix_dot(const Matrix& Qt1,const Matrix& Qt2){	
+	// Function that performs matrix matrix dot product of 3x3 matrices
+	//
+	// Args:
+	// 	Qt1(LinAlg3x3::Matrix -> std::array<std::array<double,3>,3>: Q tensor 1
+	// 	Qt2(LinAlg3x3::Matrix -> std::array<std::array<double,3>,3>: Q tensor 2
+	//
+	// returns:
+	// 	res(LinAlg3x3::Matrix -> std::array<std::array<double,3>,3>: Q1*Q2
+	
+	
+	// Initialize matrix to a matrix of 0's
+	Matrix res;	
+	// default initializes the array
+	res.fill({});
+		
+	// perform matrix matrix multiplication (simpliest N^3 algorithm but it's only 27 = 3x3x3)
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			for(int k=0;k<3;k++){
+				res[i][j] += Qt1[i][k] * Qt2[k][j]; 
+			}
+		}	
+	}
+	return res;
+}
+
+std::pair<LinAlg3x3::Real3,LinAlg3x3::Matrix> LinAlg3x3::EigenSolver(const Matrix& Q){
+	Eigen::Map<Eigen::Matrix3f> Qeigen(const_cast<Real*>(Q[0].data()),3,3);
+	Eigen::EigenSolver<Eigen::Matrix3f> eigensolver;
+	eigensolver.compute(Qeigen);
+
+	Eigen::Vector3f eigenvalue = eigensolver.eigenvalues().real();
+	Eigen::Matrix3f eigenvec   = eigensolver.eigenvectors().real();
+
+    // copy eigenvalue
+	Real3 eigval;
+	for (int i=0;i<3;i++)
+	{
+		eigval[i] = eigenvalue[i];
+	}
+
+    // copy eigenvector
+    Matrix eigvec;
+    for (int i=0;i<N_DIM;i++)
+    {
+        for (int j=0;j<N_DIM;j++)
+        {
+            eigvec[i][j] = eigenvec(i,j);
+        }
+    }
+
+    // initialize return value
+    std::pair<Real3, Matrix> ret;
+    ret.first = eigval;
+    ret.second = eigvec;
+
+    return ret;
+}
+
+std::pair<LinAlg3x3::Real3, LinAlg3x3::Matrix> LinAlg3x3::OrderEigenSolver(const Matrix& Q){
+    auto res = EigenSolver(Q);
+	std::vector<size_t> order = LinAlg3x3::argsort(res.first);
+	std::pair<Real3,Matrix> ordered_eig;
+
+    // find the ordered eigenvector
+	for(int i=0;i<3;i++){
+		ordered_eig.first[i] = res.first[order[i]];
+		for(int j=0;j<3;j++){
+			ordered_eig.second[j][i] = res.second[j][order[i]];
+		}
+	}
+
+ 	return ordered_eig;	
+}
+
+std::vector<size_t> LinAlg3x3::argsort(const Real3& array) {
+    std::vector<size_t> indices(array.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    std::sort(indices.begin(), indices.end(),
+              [&array](int left, int right) -> bool {
+                  // sort indices according to corresponding array element
+                  return array[left] > array[right];
+              });
+
+    return indices;
+}
--- OrderParameters/LinAlgTools.h
+++ OrderParameters/LinAlgTools.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "tools/CommonTypes.h"
 #include "tools/Assert.h"
+#include "Eigen/Core"
+#include "Eigen/Dense"
 
 #include <cmath>
+#include <algorithm>
+#include <numeric>
 
 namespace LinAlg3x3
 {
     using Real = CommonTypes::Real;
     using Matrix = CommonTypes::Matrix;
     using Real3= CommonTypes::Real3;
+    static constexpr int N_DIM = 3;
 
     Real3 CrossProduct(const Real3& v1, const Real3& v2);
     Real DotProduct(const Real3& v1, const Real3& v2);
@@ -17,7 +22,6 @@ namespace LinAlg3x3
     Matrix dyad(const Real3& v1, const Real3& v2);
 
     Real3 MatrixDotVector(const Matrix& mat1, const Real3& v1); 
-    
 
     // This is the rotation matrix that rotates vector 1 onto vector 2
     Matrix GetRotationMatrix(const Real3& v1, const Real3& v2);
@@ -25,4 +29,34 @@ namespace LinAlg3x3
 
     // calculate 3uiuj - I 
     Matrix LocalQtensor(const Real3& v1);
+
+	// Miscellaneous vector stuff
+	Real3  vec_add(const Real3&, const Real3&);
+	Real3  vec_add(const Real3&, const Real);
+	Real3  vec_add(const Real, const Real3&);
+	Real3  vec_sub(const Real3&, const Real3&);
+	Real3  vec_mult(const Real3&, const Real3&);
+	Real3  vec_mult(const Real3&, const Real);
+	Real3  vec_mult(const Real, const Real3&);
+	Real   vec_dot(const Real3&, const Real3&);
+	Real   vec_norm(const Real3);
+	Matrix vec_dyadic(const Real3&, const Real3&);
+	Real3	vec_cross(const Real3&, const Real3&);
+
+	// Miscellaneous matrix stuff
+	Real   matrix_trace(const Matrix&);
+	Matrix matrix_add(const Matrix&, const Matrix&);
+	Matrix matrix_sub(const Matrix&, const Matrix&);
+	Matrix matrix_Identity();
+	Matrix matrix_dot(const Matrix&,const Matrix&);
+	void   matrix_mult_inplace(Matrix&,Real);
+	void   matrix_zero_inplace(Matrix&);
+	void   matrix_accum_inplace(Matrix&, const Matrix&);
+
+	// Function that returns p2 & corresponding v1 (eigenvectors that corresponds to second largest eigenvalue of Q matrix)
+	std::pair<Real3,Matrix> EigenSolver(const Matrix& Q);
+	std::pair<Real3, Matrix> OrderEigenSolver(const Matrix& Q);
+
+	// Perform argsort of a Real3 array
+	std::vector<size_t> argsort(const Real3& array);
 }
