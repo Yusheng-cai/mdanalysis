581de494325a7f2a09b39cc44a01802dccfd6135
Yusheng Cai <cys9741@seas.upenn.edu>
2022-12-13 18:21:25 -0500

    Updated codes

---
+++ Actions/mda_tools.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "tools/CommandLineArguments.h"
+#include "tools/CommonOperations.h"
+#include "tools/CommonOperations.h"
+#include "tools/InputParser.h"
+#include "NanoparticleGeneration.hpp"
+#include "xdr/GroFile.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cstdio>
+
+namespace mda_tools{
+    using Real = CommonTypes::Real;
+    using Real3= CommonTypes::Real3;
+
+    void WriteGroFile(const std::string& filename, const std::vector<Real3>& vecpos, const std::vector<std::string>& aname, const std::vector<std::string>& resname, \
+                      const Real3& boxsize);
+}
